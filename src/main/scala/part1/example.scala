cb06c257b98f710b0ed70bc0058da82860616e99
Rob Kelly <robert.kelly@hpe.com>
2020-05-28 13:01:29 -0400

    final changes

--- src/main/scala/part1/example.scala
+++ src/main/scala/part1/example.scala
@@ -1,26 +1,30 @@
 package part2
 
-import java.nio.file.Paths
-import java.util.concurrent._
-
 import cats.effect._
 import fs2._
-import fs2.io.file
-// import scala.concurrent.ExecutionContext
-import scala.concurrent.ExecutionContext.global
-
-import org.http4s.client.{Client, JavaNetClientBuilder}
-import org.http4s.{Request, UrlForm, Uri}
-import org.http4s.Uri.uri
-import org.http4s.Method.{GET, POST}
 
 
 object Part1 extends IOApp {
   def run(args: List[String]) : IO[ExitCode] = {
-        merge()
+        merge1()
+          .covary[IO]
+          .compile
+          .drain
+          .as(ExitCode.Success)
   }
 
-  def merge[F[_]: ContextShift](): IO[ExitCode] = {
+  def merge1(): Stream[Pure, String] = {
+    fs2.Stream
+      .emits('A' to 'E')
+      .map(letter =>
+        Stream.emits(1 to 10)
+          .map(index => s"$letter$index")
+      )
+      .flatten
+      .take(5)
+  }
+
+  def merge2(): Stream[IO, Unit] = {
     fs2.Stream
       .emits('A' to 'E')
       .map(letter =>
@@ -30,9 +34,6 @@ object Part1 extends IOApp {
       .flatten
       .flatMap(x => Stream.eval(IO(println(x))))
       .take(5)
-      .compile
-      .drain
-      .as(ExitCode.Success)
   }
 
 }
--- src/main/scala/part3/example.scala
+++ src/main/scala/part3/example.scala
@@ -1,37 +1,40 @@
 package part2
 
-import java.nio.file.Paths
-import java.util.concurrent._
-
 import cats.effect._
 import fs2._
-import fs2.io.file
-// import scala.concurrent.ExecutionContext
-import scala.concurrent.ExecutionContext.global
-
-import org.http4s.client.{Client, JavaNetClientBuilder}
-import org.http4s.{Request, UrlForm, Uri}
-import org.http4s.Uri.uri
-import org.http4s.Method.{GET, POST}
 
 
 object Part3 extends IOApp {
   def run(args: List[String]) : IO[ExitCode] = {
     pipeExample()
+      .through(asyncPipeTap)
+      // .through(asyncPipeMap)
+      .compile
+      .drain
+      .as(ExitCode.Success)
   }
 
   def change[F[_]]: Pipe[F, Int, String] = {
-    _.flatMap(s => Stream(s.toString + "."))
+    _.flatMap(s => Stream(s.toString + "...."))
   }
 
-  def pipeExample() : IO[ExitCode] = {
+  def asyncPipeTap[F[_] : Sync: LiftIO]: Stream[F, String] => Stream[F, String] =
+    // Shift gives the chance to use another thread
+    _.evalTap { value =>
+      (IO.shift *> IO(println(s"Processing $value by ${Thread.currentThread().getName}"))).runAsync(
+        _ => IO.unit).to[F]
+    }
+
+  def asyncPipeMap[F[_] : Sync: LiftIO]: Stream[F, String] => Stream[F, Unit] =
+    _.evalMap { value =>
+      (IO.shift *> IO(println(s"Processing $value by ${Thread.currentThread().getName}"))).runAsync(
+        _ => IO.unit).to[F]
+    }
+
+  def pipeExample() : Stream[IO, String] = {
     fs2.Stream
       .emits(1 to 10)
         .through(change)
-        .flatMap(x => Stream.eval(IO(println(x))))
-        .compile
-        .drain
-        .as(ExitCode.Success)
+        // .flatMap(x => Stream.eval(IO(println(x))))
   }
-
 }
